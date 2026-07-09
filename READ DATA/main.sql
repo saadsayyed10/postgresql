80b346d12c455dd5170fe7895103ea83e078c0db
Saad Sayyed <saadsyed950@gmail.com>
2026-07-09 11:08:07 +0530

    Read data from tables

---
+++ READ DATA/main.sql
@@ -0,0 +1,11 @@
+-- Get all columns from a table
+SELECT * FROM person;
+
+-- Get single column from a table
+SELECT firstName from person;
+
+-- Get multiple columns from a table
+SELECT (firstName, lastName, email) from person;
+
+-- Get no column from a table
+SELECT from person;
