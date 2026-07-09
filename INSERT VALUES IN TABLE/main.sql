6b675b691048c8806f28e0e28395f8be88ae190f
Saad Sayyed <saadsyed950@gmail.com>
2026-07-09 10:24:15 +0530

    Insert values in table

---
+++ INSERT VALUES IN TABLE/main.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (firstName, lastName, gender, dateOfBirth)
+VALUES ('Anne', 'Smith', 'FEMALE', DATE ('1988-01-09'));
+
+INSERT INTO person (firstName, lastName, gender, dateOfBirth, email)
+VALUES ('Jake', 'Collins', 'MALE', DATE ('1979-10-02'), 'jake@gmail.com');
